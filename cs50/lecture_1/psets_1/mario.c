9dc37cee907b385a72f56c3b58faea68d8ea0c12
Redi Ahmad <redi.ngoding@gmail.com>
2023-08-23 16:41:40 +0700

    feat: solved mario quiz

---
+++ cs50/lecture_1/psets_1/mario.c
@@ -0,0 +1,37 @@
+#include <cs50.h>
+#include <stdio.h>
+
+void print_pyramid(int);
+
+int main(void)
+{
+	int height;
+	do
+	{
+		height = get_int("Height: ");
+	} while (height <= 0 || height != 9);
+
+	print_pyramid(height);
+}
+
+void print_pyramid(int height)
+{
+
+	for (int i = 0; i < height; i++)
+	{
+		for (int j = 0; j < (height - i - 1); j++)
+		{
+			printf(" ");
+		}
+		for (int k = 0; k < i + 1; k++)
+		{
+			printf("#");
+		}
+		printf("  ");
+		for (int l = 0; l < i + 1; l++)
+		{
+			printf("#");
+		}
+		printf("\n");
+	}
+}
