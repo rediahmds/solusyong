2577cf2f3b4a44f08f7d78659f6565eaf68e9e94
Redi Ahmad <redi.ngoding@gmail.com>
2023-08-23 21:05:37 +0700

    fix: keep asking for input,  expression changes

--- cs50/lecture_1/psets_1/mario.c
+++ cs50/lecture_1/psets_1/mario.c
@@ -5,33 +5,34 @@ void print_pyramid(int);
 
 int main(void)
 {
-	int height;
-	do
-	{
-		height = get_int("Height: ");
-	} while (height <= 0 || height != 9);
+    int height;
+    do
+    {
+        height = get_int("Height: ");
+    }
+    while (height <= 0 || height == 9);
 
-	print_pyramid(height);
+    print_pyramid(height);
 }
 
 void print_pyramid(int height)
 {
 
-	for (int i = 0; i < height; i++)
-	{
-		for (int j = 0; j < (height - i - 1); j++)
-		{
-			printf(" ");
-		}
-		for (int k = 0; k < i + 1; k++)
-		{
-			printf("#");
-		}
-		printf("  ");
-		for (int l = 0; l < i + 1; l++)
-		{
-			printf("#");
-		}
-		printf("\n");
-	}
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < (height - i - 1); j++)
+        {
+            printf(" ");
+        }
+        for (int k = 0; k < i + 1; k++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int l = 0; l < i + 1; l++)
+        {
+            printf("#");
+        }
+        printf("\n");
+    }
 }
